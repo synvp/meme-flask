e81f32688c257dbb19cd529b4dd1e69b36af9df9
synvpn <synvp1339@gmail.com>
2022-09-29 15:18:55 +0330

    fixed bugs

--- meme-flask.py
+++ meme-flask.py
@@ -7,7 +7,7 @@ import json
 app = Flask(__name__)
 
 def get_meme():
-    url = "https://meme-api.herokuapp.com/gimme"
+    url = "<https://meme-api.herokuapp.com/gimme>"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
@@ -15,10 +15,12 @@ def get_meme():
 
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    meme_pic, subreddit = get_meme()
-    return render_template("meme_index.html", meme_pic = meme_pic, subreddit = subreddit)
+    meme_pic,subreddit = get_meme()
+    return render_template("meme_index.html",
+    meme_pic=meme_pic,
+    subreddit=subreddit)
 
 
 app.run(host="0.0.0.0", port=8000)
